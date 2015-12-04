bbcecbb247b128995e9a2418f47175fc67d0b8dd
Sadie Donnelly <sadieolivia01@gmail.com>
2015-12-04 15:09:00 -0600

    End of Video 6.3.1

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,5 +1,8 @@
 #! /usr/bin/env ruby
 
-asm_filename = ARGV[0]
-
-puts asm_filename
+if(ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1 && File.exist?(ARGV[0]) && File.readable?(ARGV[0]))
+	asm_filename = ARGV[0]
+	puts asm_filename
+else
+	abort("Usage: ./assembler.rb Prog.asm")
+end
