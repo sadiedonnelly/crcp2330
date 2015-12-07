bd7b553e33c71b0b666fc379ae551d594a725843
Sadie Donnelly <sadieolivia01@gmail.com>
2015-12-07 10:15:31 -0600

    Add parser.

---
+++ 06/parser.rb
@@ -0,0 +1,11 @@
+class Parser
+	def initialize(assembly_instructions)
+		@assembly_instructions = assembly_instructions
+		@machine_instructions = []
+	end
+
+	def parse
+		#TODO
+		@machine_instructions
+	end	
+end
